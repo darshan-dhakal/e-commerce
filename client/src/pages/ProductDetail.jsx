e228a8cacff1cdc46fbbe3c18d8353508b3a34da
Darshan Dhakal <Darshan Dhakal>
2025-07-10 17:56:20 +0545

    updated product detail

---
+++ client/src/pages/ProductDetail.jsx
@@ -0,0 +1,10 @@
+import Layout from "../layouts/Layouts";
+import ProductDescription from "../components/ProductDetail";
+function ProductDetail() {
+  return (
+    <Layout>
+      <ProductDescription></ProductDescription>
+    </Layout>
+  );
+}
+export default ProductDetail;
